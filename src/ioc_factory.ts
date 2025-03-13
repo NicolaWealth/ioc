c7d9a39639c475ef271216c4cf4506c618235281
Skye Haik <shaik@nicolawealth.com>
2025-03-13 10:55:26 -0700

    fix error in get() function in if statement conditional for error trap

--- src/ioc_factory.ts
+++ src/ioc_factory.ts
@@ -12,7 +12,7 @@ export const iocFactory = () => {
   // lookup by name and return _current.
   // validates a default exists, as we'd only use this to lookup things that should exist
   const get = <T>(name: string): T => {
-    if (_current[name] === undefined || _current[name] === undefined) {
+    if (_current[name] === undefined || _defaults[name] === undefined) {
       throw new Error(`ioc.get("${name}") no current or default defined.`);
     }
     return _current[name] as T;
